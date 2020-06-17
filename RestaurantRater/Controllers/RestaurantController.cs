3438fd60c2057a05f4e0e40d917820ef7bc9b5fc
Todd Newland <tnewland08@gmail.com>
2020-06-16 20:58:33 -0400

    added Edit methods and view

--- RestaurantRater/Controllers/RestaurantController.cs
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -1,8 +1,10 @@
 ﻿using RestaurantRater.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
+using System.Runtime.CompilerServices;
 using System.Web;
 using System.Web.Mvc;
 
@@ -63,6 +65,40 @@ namespace RestaurantRater.Controllers
             _db.Restaurants.Remove(restaurant);
             _db.SaveChanges();
             return RedirectToAction("Index");
+        }
+
+        // GET: Restaurant/Edit/{id}
+        // Get an id from the user
+        // Handle if the id is null
+        // Find a restaurant by that id
+        // If the restaurant doesn't exist
+        // Return the restaurant and the view
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            if (restaurant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurant);
+        }
+
+        // POST: Restaurant/Edit/{id}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(restaurant).State = EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(restaurant);
         }
     }
 }
